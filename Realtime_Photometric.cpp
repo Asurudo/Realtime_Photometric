0b645abeea0657d41af42b0e43fd132cc59900af
Asurudo Jyo <asurudo@126.com>
2024-06-28 12:14:52 +0900

    init

---
+++ Realtime_Photometric.cpp
@@ -0,0 +1,472 @@
+﻿#include<iostream>
+#include<cstdio>
+#include <filesystem>
+
+#define GLAD_GL_IMPLEMENTATION
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
+#include "shader.h"
+#include "camera.h"
+#include "model.h"
+#include "mesh.h"
+#include "colors.hpp"
+#include "ltc_matrix.hpp"
+
+// 屏幕大小
+const unsigned int SCR_WIDTH = 1280;
+const unsigned int SCR_HEIGHT = 720;
+
+// 光源
+const glm::vec3 LIGHT_COLOR = Color::White; // CHANGE AREA LIGHT COLOR HERE!
+glm::vec3 areaLightTranslate;
+Shader* ltcShaderPtr;
+
+// 控制
+bool keys[1024]; // activated keys
+
+// 摄像机
+Camera camera(glm::vec3(0.0f, 1.0f, 0.5f), glm::vec3(0.0f, 1.0f, 0.0f), 180.0f, 0.0f);
+float lastX = (float)SCR_WIDTH / 2.0f;
+float lastY = (float)SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// 时间
+float deltaTime = 0.0f;	// 这一帧与上一帧之间的间隔时间
+float lastFrame = 0.0f;
+
+// 面光源与平面定义
+// 2---3-5
+// |  / /|
+// | / / |
+// |/ /  |
+// 1-4---6
+//
+GLuint planeVBO, planeVAO;
+GLuint areaLightVBO, areaLightVAO;
+struct VertexAL {
+	glm::vec3 position;
+	glm::vec3 normal;
+	glm::vec2 texcoord;
+};
+
+const GLfloat psize = 10.0f;
+VertexAL planeVertices[6] = {
+	{ {-psize, 0.0f, -psize}, {0.0f, 1.0f, 0.0f}, {0.0f, 0.0f} },
+	{ {-psize, 0.0f,  psize}, {0.0f, 1.0f, 0.0f}, {0.0f, 1.0f} },
+	{ { psize, 0.0f,  psize}, {0.0f, 1.0f, 0.0f}, {1.0f, 1.0f} },
+	{ {-psize, 0.0f, -psize}, {0.0f, 1.0f, 0.0f}, {0.0f, 0.0f} },
+	{ { psize, 0.0f,  psize}, {0.0f, 1.0f, 0.0f}, {1.0f, 1.0f} },
+	{ { psize, 0.0f, -psize}, {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f} }
+};
+VertexAL areaLightVertices[6] = {
+	{ {-8.0f, 2.4f, -1.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f} }, // 0 1 5 4
+	{ {-8.0f, 2.4f,  1.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 1.0f} },
+	{ {-8.0f, 0.4f,  1.0f}, {1.0f, 0.0f, 0.0f}, {1.0f, 1.0f} },
+	{ {-8.0f, 2.4f, -1.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f} },
+	{ {-8.0f, 0.4f,  1.0f}, {1.0f, 0.0f, 0.0f}, {1.0f, 1.0f} },
+	{ {-8.0f, 0.4f, -1.0f}, {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f} }
+};
+
+void configureMockupData()
+{
+	// PLANE
+	glGenVertexArrays(1, &planeVAO);
+	glGenBuffers(1, &planeVBO);
+
+	glBindVertexArray(planeVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), planeVertices, GL_STATIC_DRAW);
+
+	// position
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat),
+		(GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	// normal
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat),
+		(GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+	// texcoord
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat),
+		(GLvoid*)(6 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(2);
+	glBindVertexArray(0);
+
+	// AREA LIGHT
+	glGenVertexArrays(1, &areaLightVAO);
+	glBindVertexArray(areaLightVAO);
+
+	glGenBuffers(1, &areaLightVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, areaLightVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(areaLightVertices), areaLightVertices, GL_STATIC_DRAW);
+
+	// position
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat),
+		(GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	// normal
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat),
+		(GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+	// texcoord
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat),
+		(GLvoid*)(6 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(2);
+	glBindVertexArray(0);
+
+	glBindVertexArray(0);
+}
+void renderPlane()
+{
+	glBindVertexArray(planeVAO);
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+	glBindVertexArray(0);
+}
+void renderAreaLight()
+{
+	glBindVertexArray(areaLightVAO);
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+	glBindVertexArray(0);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void do_movement(GLfloat deltaTime);
+
+struct LTC_matrices {
+	GLuint mat1;
+	GLuint mat2;
+};
+
+GLuint loadMTexture()
+{
+	GLuint texture = 0;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 64, 64,
+		0, GL_RGBA, GL_FLOAT, LTC1);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+	return texture;
+}
+GLuint loadLUTTexture()
+{
+	GLuint texture = 0;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 64, 64,
+		0, GL_RGBA, GL_FLOAT, LTC2);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+	return texture;
+}
+
+void incrementRoughness(float step)
+{
+	static glm::vec3 color = Color::White;
+	static float roughness = 0.5f;
+	roughness += step;
+	roughness = glm::clamp(roughness, 0.0f, 1.0f);
+	//std::cout << "roughness: " << roughness << '\n';
+	ltcShaderPtr->use();
+	ltcShaderPtr->setVec4("material.albedoRoughness", glm::vec4(color, roughness));
+	glUseProgram(0);
+}
+void incrementLightIntensity(float step)
+{
+	static float intensity = 4.0f;
+	intensity += step;
+	intensity = glm::clamp(intensity, 0.0f, 10.0f);
+	//std::cout << "intensity: " << intensity << '\n';
+	ltcShaderPtr->use();
+	ltcShaderPtr->setFloat("areaLight.intensity", intensity);
+	glUseProgram(0);
+}
+void switchTwoSided(bool doSwitch)
+{
+	static bool twoSided = true;
+	if (doSwitch) twoSided = !twoSided;
+	//std::cout << "twoSided: " << std::boolalpha << twoSided << '\n';
+	ltcShaderPtr->use();
+	ltcShaderPtr->setFloat("areaLight.twoSided", twoSided);
+	glUseProgram(0);
+}
+
+int main()
+{	
+	// OpenGL与窗口的初始化 -------------------------------------------------------------------------------------------------start
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "hoge", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	const int version = gladLoadGL(glfwGetProcAddress);
+	if (version == 0) {
+		fprintf(stderr, "Failed to load OpenGL 3.x/4.x libraries!\n");
+		return 1;
+	}
+	printf("Load OpenGL %d.%d\n", GLAD_VERSION_MAJOR(version), GLAD_VERSION_MINOR(version));
+
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+	glfwSetKeyCallback(window, key_callback);
+
+	// 隐藏鼠标
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+
+	// imgui库
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext(); //创建上下文
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // 允许键盘控制
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;  // 允许游戏手柄控制
+
+	// 设置渲染器后端
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init();
+	// OpenGL与窗口的初始化 -------------------------------------------------------------------------------------------------end
+
+	// 预处理 -------------------------------------------------------------------------------------------------start
+	glEnable(GL_DEPTH_TEST);
+	// LUT textures
+	LTC_matrices mLTC;
+	mLTC.mat1 = loadMTexture();
+	mLTC.mat2 = loadLUTTexture();
+
+	// SHADERS
+	Shader shaderPlane("../../../glsl/plane.vert", "../../../glsl/plane.frag");
+	ltcShaderPtr = &shaderPlane;
+	Shader shaderLight("../../../glsl/area_light.vert", "../../../glsl/area_light.frag");
+
+	// SHADER CONFIGURATION
+	shaderPlane.use();
+	shaderPlane.setVec3("areaLight.points[0]", areaLightVertices[0].position);
+	shaderPlane.setVec3("areaLight.points[1]", areaLightVertices[1].position);
+	shaderPlane.setVec3("areaLight.points[2]", areaLightVertices[4].position);
+	shaderPlane.setVec3("areaLight.points[3]", areaLightVertices[5].position);
+	shaderPlane.setVec3("areaLight.color", LIGHT_COLOR);
+	shaderPlane.setInt("LTC1", 0);
+	shaderPlane.setInt("LTC2", 1);
+	shaderPlane.setInt("material.diffuse", 2);
+	incrementRoughness(0.0f);
+	incrementLightIntensity(0.0f);
+	switchTwoSided(false);
+	glUseProgram(0);
+
+	shaderLight.use();
+	glm::mat4 model(1.0f);
+	shaderLight.setMat4("model", model);
+	shaderLight.setVec3("lightColor", LIGHT_COLOR);
+	glUseProgram(0);
+
+	// 3D OBJECTS
+	configureMockupData();
+	areaLightTranslate = glm::vec3(0.0f, 0.0f, 0.0f);
+	// 预处理 -------------------------------------------------------------------------------------------------end
+	
+	while (!glfwWindowShouldClose(window))
+	{
+		// 处理时间、输入以及清屏 --------------------------------------------------------------------------------------------------start
+		float currentFrame = static_cast<float>(glfwGetTime());
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		glfwPollEvents();
+		do_movement(deltaTime);
+
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+		// 处理时间、输入以及清屏 --------------------------------------------------------------------------------------------------end
+
+		shaderPlane.use();
+		glm::mat4 model(1.0f);
+		glm::mat3 normalMatrix = glm::mat3(model);
+		shaderPlane.setMat4("model", model);
+		shaderPlane.setMat3("normalMatrix", normalMatrix);
+		glm::mat4 view = camera.GetViewMatrix();
+		shaderPlane.setMat4("view", view);
+		glm::mat4 projection = glm::perspective(
+		glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+		shaderPlane.setMat4("projection", projection);
+		shaderPlane.setVec3("viewPosition", camera.Position);
+		shaderPlane.setVec3("areaLightTranslate", areaLightTranslate);
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, mLTC.mat1);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, mLTC.mat2);
+		renderPlane();
+		glUseProgram(0);
+
+		shaderLight.use();
+		model = glm::translate(model, areaLightTranslate);
+		shaderLight.setMat4("model", model);
+		shaderLight.setMat4("view", view);
+		shaderLight.setMat4("projection", projection);
+		renderAreaLight();
+		glUseProgram(0);
+
+		ImGui::Text("Avg fps: %.3f", ImGui::GetIO().Framerate);
+		ImGui::SliderFloat("Degree", &areaLightTranslate.z, 0.f, 100.f);
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		glfwSwapBuffers(window);
+	}
+	glfwTerminate();
+	return 0;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	static unsigned short wireframe = 0;
+
+	if (action == GLFW_PRESS)
+	{
+		switch (key)
+		{
+		case GLFW_KEY_ESCAPE:
+			glfwSetWindowShouldClose(window, GL_TRUE);
+			return;
+		case GLFW_KEY_B:
+			switchTwoSided(true);
+			break;
+		default:
+			keys[key] = true;
+			break;
+		}
+	}
+
+	if (action == GLFW_RELEASE)
+	{
+		if (key == GLFW_KEY_SPACE) {
+			switch (wireframe)
+			{
+			case 0:
+				glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+				wireframe = 1;
+				break;
+			default:
+				glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+				wireframe = 0;
+				break;
+			}
+		}
+		else {
+			keys[key] = false;
+		}
+	}
+}
+void do_movement(GLfloat deltaTime)
+{
+	float cameraSpeed = deltaTime * 3.0f;
+
+	if (keys[GLFW_KEY_W]) {
+		camera.ProcessKeyboard(FORWARD, cameraSpeed);
+	}
+	else if (keys[GLFW_KEY_S]) {
+		camera.ProcessKeyboard(BACKWARD, cameraSpeed);
+	}
+	if (keys[GLFW_KEY_A]) {
+		camera.ProcessKeyboard(LEFT, cameraSpeed);
+	}
+	else if (keys[GLFW_KEY_D]) {
+		camera.ProcessKeyboard(RIGHT, cameraSpeed);
+	}
+
+	if (keys[GLFW_KEY_R]) {
+		if (keys[GLFW_KEY_LEFT_SHIFT]) incrementRoughness(0.01f);
+		else incrementRoughness(-0.01f);
+	}
+
+	if (keys[GLFW_KEY_I]) {
+		if (keys[GLFW_KEY_LEFT_SHIFT]) incrementLightIntensity(0.025f);
+		else incrementLightIntensity(-0.025f);
+	}
+
+	if (keys[GLFW_KEY_LEFT]) {
+		areaLightTranslate.z += 0.01f;
+	}
+	if (keys[GLFW_KEY_RIGHT]) {
+		areaLightTranslate.z -= 0.01f;
+	}
+	if (keys[GLFW_KEY_UP]) {
+		areaLightTranslate.y += 0.01f;
+	}
+	if (keys[GLFW_KEY_DOWN]) {
+		areaLightTranslate.y -= 0.01f;
+	}
+}
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+}
+// glfw: whenever the mouse moves, this callback is called
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+	float xpos = static_cast<float>(xposIn);
+	float ypos = static_cast<float>(yposIn);
+
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(static_cast<float>(yoffset));
+}
---
+++ src/texture.h
@@ -0,0 +1,105 @@
+﻿#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+class Texture
+{
+	public:
+		unsigned int ID;
+		std::string path;
+		std::string type;
+		// 储存纹理的路径用于与其它纹理进行比较
+		Texture(unsigned int id, const std::string &s, const std::string &t) :  ID(id), path(s), type(t) {}
+		static unsigned int loadTexture(char const* path)
+		{
+			unsigned int textureID;
+			glGenTextures(1, &textureID);
+
+			int width, height, nrComponents;
+			unsigned char* data = stbi_load(path, &width, &height, &nrComponents, 0);
+			if (data)
+			{
+				GLenum format;
+				if (nrComponents == 1)
+					format = GL_RED;
+				else if (nrComponents == 3)
+					format = GL_RGB;
+				else if (nrComponents == 4)
+					format = GL_RGBA;
+
+				glBindTexture(GL_TEXTURE_2D, textureID);
+				glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+				glGenerateMipmap(GL_TEXTURE_2D);
+
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT); // for this tutorial: use GL_CLAMP_TO_EDGE to prevent semi-transparent borders. Due to interpolation it takes texels from next repeat 
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+				stbi_image_free(data);
+			}
+			else
+			{
+				std::cout << "Texture failed to load at path: " << path << std::endl;
+				stbi_image_free(data);
+			}
+
+			return textureID;
+		}
+		static unsigned int loadTexture(char const* path, bool gammaCorrection)
+		{
+			unsigned int textureID;
+			glGenTextures(1, &textureID);
+
+			int width, height, nrComponents;
+			unsigned char* data = stbi_load(path, &width, &height, &nrComponents, 0);
+			if (data)
+			{
+				GLenum internalFormat;
+				GLenum dataFormat;
+				if (nrComponents == 1)
+				{
+					internalFormat = dataFormat = GL_RED;
+				}
+				else if (nrComponents == 3)
+				{
+					internalFormat = gammaCorrection ? GL_SRGB : GL_RGB;
+					dataFormat = GL_RGB;
+				}
+				else if (nrComponents == 4)
+				{
+					internalFormat = gammaCorrection ? GL_SRGB_ALPHA : GL_RGBA;
+					dataFormat = GL_RGBA;
+				}
+
+				glBindTexture(GL_TEXTURE_2D, textureID);
+				glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, width, height, 0, dataFormat, GL_UNSIGNED_BYTE, data);
+				glGenerateMipmap(GL_TEXTURE_2D);
+
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+				stbi_image_free(data);
+			}
+			else
+			{
+				std::cout << "Texture failed to load at path: " << path << std::endl;
+				stbi_image_free(data);
+			}
+
+			return textureID;
+		}
+};
+#endif
